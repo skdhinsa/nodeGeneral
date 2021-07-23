64e457c5b029326ea1271ce6e7981fc3d890d1cf
Serena <Serena@MacBook-Air.local>
2021-07-23 12:04:40 +0100

    routing in node

---
+++ routing.js
@@ -0,0 +1,15 @@
+// set up routing system
+const http = require('http');
+const fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+    console.log('request was made ' + req.url); // req.url -> where user is sending request
+    // when we make a req to server, write header on respond obj
+    if (req.url === '/' || req.url === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
+    }
+});
+
+server.listen(8000, () => { "Listening on port 8000" });
+

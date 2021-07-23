1309917b7d465bd88a6ad1c9ed33709069cfb58f
Serena <Serena@MacBook-Air.local>
2021-07-23 14:08:48 +0100

    changed structure

--- basics/files.js
+++ basics/files.js
@@ -2,18 +2,18 @@
 const fs = require('fs');
 
 // reading files - args (file path, func to invoke when complete)
-// fs.readFile('./files/random.txt', (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(data.toString());
-//     }
-// });
+fs.readFile('../files/random.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data.toString());
+    }
+});
 
 // writing files - args (path, text to write to file, callback func)
-// fs.writeFile('./files/random2.txt', "Writing to file", () => {
-//     console.log("C'est fini");
-// })
+fs.writeFile('./files/random2.txt', "Writing to file", () => {
+    console.log("C'est fini");
+})
 
 // directories 
 // sync method - blocks code

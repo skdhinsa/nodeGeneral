c489519c7080fa64b54198636aa9c3bcda345abf
Serena <Serena@MacBook-Air.local>
2021-07-23 12:15:57 +0100

    routing to different endpoints

--- routing.js
+++ routing.js
@@ -8,6 +8,9 @@ var server = http.createServer(function (req, res) {
     if (req.url === '/' || req.url === '/home') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/index.html').pipe(res)
+    } else if (req.url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/contact.html').pipe(res)
     }
 });
 

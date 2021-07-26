dc660a2feddcf2b4177d0060f26e197329a0b80b
Serena <Serena@MacBook-Air.local>
2021-07-26 16:02:56 +0100

    status codes and redirecting

--- routingSystem.js
+++ routingSystem.js
@@ -4,17 +4,30 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
     res.setHeader('Content-Type', 'text/html');
     let path = './views/';
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/ABOUT':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        case '/contact':
+            path += 'contact.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 

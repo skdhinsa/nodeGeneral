a51fa10bcec41188205d6584536f56e9dc3f0efc
Serena <Serena@MacBook-Air.local>
2021-07-26 13:57:42 +0100

    sending html files to the browser

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+const fs = require('fs');
 const http = require('http');
-const { dirname } = require('path');
+
 
 // get access to request and response objects
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>This is a HTML response</h1>')
-    res.write('<p>Sending a response.</p>')
-    res.end();
-
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost', console.log('Listening for requests on port 3000'));
